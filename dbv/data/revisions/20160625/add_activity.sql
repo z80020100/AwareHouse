a8ac1edba56ba804a27e43ca3e9200f1f561f108
Cliff Wu <z800201002005@gmaill.com>
2016-06-26 00:00:44 +0800

    Add database version control tools: dbv 1.2

---
+++ dbv/data/revisions/20160625/add_activity.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `main` ADD activity bool DEFAULt true;
+ALTER TABLE `required_option` ADD activity bool DEFAULt true;
+ALTER TABLE `series` ADD activity bool DEFAULt true;
+ALTER TABLE `additional_type` ADD activity bool DEFAULt true;
+ALTER TABLE `additional_item` ADD activity bool DEFAULt true;
