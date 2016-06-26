1b517208d627a42379b574a08284e5f5145e6f3b
sungshil <martinetlee@gmail.com>
2016-06-26 20:31:49 +0800

    integrate header and footer, putting main div into header

--- includes/header.php
+++ includes/header.php
@@ -7,9 +7,20 @@ $_HTML = '';
 
 $template = $twig->loadTemplate('header.html');
 
+if(is_admin()){
+	$_USER_IDENTITY = 'admin';
+}
+elseif(is_login()){
+	$_USER_IDENTITY = 'member';
+}
+else{
+	$_USER_IDENTITY = 'guest';
+}
+
 
 $_HTML .= $template->render(array(
 	'PAGE_TITLE' => $_PAGE_TITLE,
+	'USER_IDENTITY' => $_USER_IDENTITY,
 ));
 
 
