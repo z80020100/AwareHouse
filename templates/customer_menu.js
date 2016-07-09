8fb859e2e1d877a664c194d457385d88199451e0
isaac60103 <isaac60103@gmail.com>
2016-07-10 04:11:54 +0800

    separate html and css on customer_menu

--- templates/customer_menu.js
+++ templates/customer_menu.js
@@ -13,39 +13,6 @@ var items_array_length=0;
 
 $(document).ready(function(){
 
-        
-
-        $("button[id^='m']").css({"background-color":"#ffc952", "color":"#ff7473"});
-
-        
-        $("#nav_cross").click(function(){
-
-            $(".w3-sidenav").toggle();
-            $("#main").css({"marginLeft":"0%"});
-            $(".w3-sidenav").css({"display":"none"});
-            $(".w3-opennav").css({"display":"inline-block"});
-
-        });
-
-        $("#nav_open").click(function(){
-                            
-            $(".w3-sidenav").toggle();
-            $("#main").css({"marginLeft":"20%"});
-            $(".w3-sidenav").css({"display":"block", "width":"20%"});
-            $(".w3-opennav").css({"display":"none"});
-
-        });
-
-
-        $("#nav_open").css({"position":"fixed"});
-        /////
-        $("#open_cart").css({"position":"fixed", "right":"7%", "bottom":"7%", "z-index":"2"});
-        $("#open_cart2").css({"position":"fixed", "right":"15%", "bottom":"7%", "z-index":"2"});
-        $("#open_cart3").css({"position":"fixed", "right":"23%", "bottom":"7%", "z-index":"2"});
-        $("#open_cart4").css({"position":"fixed", "right":"31%", "bottom":"7%", "z-index":"2"});
-        //////
-        $("#nav_cross").click(); 
-
 
 
         $("#open_cart").click(function(){
