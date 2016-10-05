b086e16f4c818a6fef634ee7dcee19c63de194d5
Cliff Wu <z800201002005@gmail.com>
2016-10-06 01:44:59 +0800

    Fix admin permission for user: mrtoast

--- sql/bfproj.sql
+++ sql/bfproj.sql
@@ -1012,7 +1012,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`u_id`, `u_name`, `u_pass`, `u_type`) VALUES
-(1, 'mrtoast', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 2),
+(1, 'mrtoast', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 3),
 (2, 'potato', '1234', 0),
 (3, 'test', '', 0),
 (4, 'Yoo', '', 0),
